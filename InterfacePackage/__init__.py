44254945abf63da4d38651911a93999dd882ccd6
Long <nglong0304@gmail.com>
2021-03-28 00:40:17 +0100

    update interface package

--- InterfacePackage/__init__.py
+++ InterfacePackage/__init__.py
@@ -1,6 +1,3 @@
 from .Button import Button
-from .Button import IncrementStructure
 from .Text import Text
 from .TextEntry import TextEntry
-from .Tools import number_format
-from .Tools import str_to_dmg
