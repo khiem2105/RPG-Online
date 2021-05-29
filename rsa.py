d3e2676e6d56049b89edcf5686af97da7d1fbb3c
Khiem <khiem2105.1@gmail.com>
2021-05-29 16:01:29 +0200

    delete some useless function

--- rsa.py
+++ rsa.py
@@ -1,8 +1,5 @@
 from key import *
 
-encrypt = wrap_function("encrypt", ctypes.c_char_p, [ctypes.POINTER(ctypes.c_char), Key])
-decrypt = wrap_function("decrypt", ctypes.c_char_p, [ctypes.POINTER(ctypes.c_char), Key])
-
 def encode(s, pub_key):
     encrypt = wrap_function("encrypt", ctypes.c_char_p, [ctypes.POINTER(ctypes.c_char), Key])
     input_string = ctypes.create_string_buffer(str.encode(s))
